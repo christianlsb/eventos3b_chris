2efed63d924679d49bcec055f9a143309c9df0e6
chrislsb <chrislsb09@gmail.com>
2022-05-10 10:01:39 -0600

    commit edit
    edit

--- src/main/java/br/senai/controller/ClienteController.java
+++ src/main/java/br/senai/controller/ClienteController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.lang.ref.Cleaner;
 
 @Controller
 public class ClienteController {
@@ -41,4 +44,11 @@ public class ClienteController {
             return "cliente/add";
         }
     }
+
+    @GetMapping("/cliente/edit/{id}")
+    public String edit(Model model, @PathVariable long id) {
+       model.addAttribute("cliente", clienteService.findById(id));
+       return "cliente/edit";
+    }
+
 }
