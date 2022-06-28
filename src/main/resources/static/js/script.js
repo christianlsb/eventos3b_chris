83189e2683221ea2eef49493113366d071c6f137
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2022-06-28 20:41:18 -0300

    commit front
    commit front

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -22,3 +22,25 @@ const deletar = ()=>{
         window.location.href='/' + path_selected + '/delete' + id_selected
     }
 }
+
+
+
+//upar foto
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+}) 
+
+//upar foto
